50c81398c27d4c4a459a1abbbdd4de501f5d71a8
utfunderscore <83981186+utfunderscore@users.noreply.github.com>
2025-05-23 22:25:30 +0100

    fix: Add game to StageCreator

--- core/src/main/java/org/readutf/engine/stage/StageCreator.java
+++ core/src/main/java/org/readutf/engine/stage/StageCreator.java
@@ -1,6 +1,8 @@
 package org.readutf.engine.stage;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.readutf.engine.Game;
 import org.readutf.engine.arena.Arena;
 import org.readutf.engine.stage.exception.StageChangeException;
 import org.readutf.engine.team.GameTeam;
@@ -8,5 +10,5 @@ import org.readutf.engine.team.GameTeam;
 public interface StageCreator<ARENA extends Arena<?, ?>, TEAM extends GameTeam> {
 
     @NotNull
-    Stage<ARENA, TEAM> startNextStage(Stage<ARENA, TEAM> previousStage) throws StageChangeException;
+    Stage<ARENA, TEAM> startNextStage(@NotNull Game<ARENA, TEAM> game, @Nullable Stage<ARENA, TEAM> previousStage) throws StageChangeException;
 }
