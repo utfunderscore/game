ba19b1db44dd8af216bec10b2e7348ee6b5c5b51
utfunderscore <83981186+utfunderscore@users.noreply.github.com>
2025-05-27 17:10:51 +0100

    build: Update getListeners method to use raw ListenerData type

--- core/src/main/java/org/readutf/engine/feature/System.java
+++ core/src/main/java/org/readutf/engine/feature/System.java
@@ -1,13 +1,10 @@
 package org.readutf.engine.feature;
 
-import org.jetbrains.annotations.NotNull;
-import org.readutf.engine.event.listener.GameListener;
-import org.readutf.engine.event.listener.ListenerData;
-import org.readutf.engine.task.GameTask;
-
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
+import org.jetbrains.annotations.NotNull;
+import org.readutf.engine.event.listener.ListenerData;
+import org.readutf.engine.task.GameTask;
 
 public interface System {
 
@@ -15,7 +12,7 @@ public interface System {
      * Returns a map of event types to their corresponding listeners.
      * Subclasses can override to provide actual event listeners.
      */
-    default @NotNull List<ListenerData<?>> getListeners() {
+    default @NotNull List<ListenerData> getListeners() {
         return List.of();
     }
 
