4d84fb2afe96e92f9098eb530c153b2052b0b3ca
utfunderscore <83981186+utfunderscore@users.noreply.github.com>
2025-05-30 00:08:00 +0100

    feat: Introduce easyId for Game and update GameManager to track games by easyId

--- core/src/main/java/org/readutf/engine/GameManager.java
+++ core/src/main/java/org/readutf/engine/GameManager.java
@@ -1,8 +1,8 @@
 package org.readutf.engine;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.List;
 import java.util.UUID;
 import lombok.experimental.UtilityClass;
 import org.jetbrains.annotations.NotNull;
@@ -11,18 +11,39 @@ import org.jetbrains.annotations.Nullable;
 @UtilityClass
 public class GameManager {
 
-    private @NotNull static final Map<UUID, Game<?, ?, ?>> gameTracker = new HashMap<>();
+    private @NotNull static final HashSet<Game<?, ?, ?>> gameTracker = new HashSet<>();
 
-    public static void register(Game<?, ?, ?> game) {
-        gameTracker.put(game.getId(), game);
+    public static void register(@NotNull Game<?, ?, ?> game) {
+        gameTracker.add(game);
     }
 
-    public static @Nullable Game<?, ?, ?> getGame(UUID uuid) {
-        return gameTracker.get(uuid);
+    public static void unregister(Game<?, ?, ?> game) {
+        gameTracker.remove(game);
+    }
+
+    public static @Nullable Game<?, ?, ?> getGameByPlayer(UUID uuid) {
+        for (Game<?, ?, ?> game : gameTracker) {
+            if (game.getPlayers().contains(uuid)) {
+                return game;
+            }
+        }
+        return null;
+    }
+
+    public static @Nullable Game<?, ?, ?> getGameByName(String name) {
+        return gameTracker.stream()
+                .filter(game -> game.getEasyId().equalsIgnoreCase(name))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public static @NotNull List<String> getGameNames() {
+        return gameTracker.stream()
+                .map(Game::getEasyId)
+                .toList();
     }
 
     public static @NotNull Collection<Game<?, ?, ?>> getGames() {
-        return gameTracker.values();
+        return gameTracker;
     }
-
 }
